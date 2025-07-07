8b19721cfa9f22e554ecca38b558ad85c8ed0939
Emil Berglund <emil.berglund@live.no>
2025-07-07 13:37:29 +0200

    Add support for both docker and quasar localhost

--- varsel/src/main/java/emilb04/varsel/Controller/CorsConfig.java
+++ varsel/src/main/java/emilb04/varsel/Controller/CorsConfig.java
@@ -5,34 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/* Vue, Quasar and Maven localhost
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:9000")
-                        .allowedMethods("*");
-            }
-        };
-    }
-}
-*/
+import io.micrometer.common.lang.NonNull;
 
-/* For Docker usage */
 @Configuration
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedOrigins("http://localhost", "http://localhost:9000")
+                        .allowedMethods("*");
             }
         };
     }
