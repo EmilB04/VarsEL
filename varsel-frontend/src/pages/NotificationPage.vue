86c307265b8f03a66034162a23f282831c8be610
Emil Berglund <emil.berglund@live.no>
2025-07-07 15:17:58 +0200

    Add empty notificationpage

---
+++ varsel-frontend/src/pages/NotificationPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="q-pa-md">
+    <header>
+      <NavSection />
+      <h1 class="text-h3 text-center">Få notifikasjoner ved ønsket strømpris</h1>
+    </header>
+
+    <main>
+
+    </main>
+
+  </q-page>
+  <footer class="text-center">
+    <FooterSection />
+  </footer>
+</template>
+
+
+<script setup lang="ts">
+import FooterSection from 'src/components/FooterSection.vue'
+import NavSection from 'src/components/NavSection.vue'
+</script>
