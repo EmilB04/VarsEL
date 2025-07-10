31ae1ccf3e692f1b9345aafef79f49273230f629
Emil Berglund <emil.berglund@live.no>
2025-07-10 21:18:41 +0200

    Add popup message when switching tax-option

--- varsel-frontend/src/components/TaxMode.vue
+++ varsel-frontend/src/components/TaxMode.vue
@@ -7,25 +7,31 @@
       unchecked-icon="close"
       @update:model-value="toggleTax"
       :label="isTaxIncluded ? 'MVA inkludert' : 'MVA ekskludert'"
-    />
+    >
+      <q-popup-proxy transition-show="scale" transition-hide="scale" self="top middle">
+        <q-banner class="bg-orange text-white text-center" style="min-width: 250px;">
+          <template v-slot:avatar>
+            <q-icon name="refresh" color="white" />
+          </template>
+          Du må hente priser på nytt for å se endringene i MVA-innstillingene.
+        </q-banner>
+      </q-popup-proxy>
+    </q-toggle>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-const isTaxIncluded = ref(true)
-
-onMounted(() => {
-  const storedTax = localStorage.getItem('isTaxIncluded')
-  isTaxIncluded.value = storedTax === 'true'
-})
+import { ref, onMounted } from 'vue';
+const isTaxIncluded = ref(true);
 
 function toggleTax() {
-  localStorage.setItem('isTaxIncluded', isTaxIncluded.value.toString())
-  console.log('isTaxIncluded:', isTaxIncluded.value)
+  localStorage.setItem('isTaxIncluded', isTaxIncluded.value.toString());
 }
 onMounted(() => {
-  toggleTax()
-})
-
+  const storedTax = localStorage.getItem('isTaxIncluded');
+  isTaxIncluded.value = storedTax === 'true';
+  toggleTax();
+});
 </script>
+
+<style scoped lang="scss"></style>
